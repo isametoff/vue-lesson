c7fd9df80a83b19be7c384f4d50a623c451363d6
Vadim isametov <isametoff@gmail.com>
2022-10-11 10:02:48 +0200

    1.0

---
+++ src/components/Product.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Title</h1>
+    Back to products
+    <hr />
+    <div class="alert alert-success">
+      price
+      {{ product }}
+    </div>
+    <button
+      v-if="inCart(product.id)"
+      @click="remove(product.id)"
+      type="button"
+      class="btn btn-danger"
+    >
+      Remove
+    </button>
+    <button
+      v-else
+      @click="add(product.id)"
+      type="button"
+      class="btn btn-success"
+    >
+      Add to cart
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  mounted() {
+    console.log(this.product)
+  },
+  computed: {
+    ...mapGetters('products', ['product']),
+    ...mapGetters('cart', ['inCart']),
+  },
+  methods: {
+    ...mapActions('cart', ['add', 'remove']),
+  },
+}
+</script>
